c291791d099d17478b24e825bfe6d633ec201671
KennSecusana <kennsecusana@gmail.com>
2022-08-30 23:32:46 +0800

    Changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,12 +1,60 @@
 @extends('layouts.layout')
 
 @section('content')
-<div class="container" style="margin-top:150px;">
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>{{ config('app.name') }}</title>
+
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no" name="viewport">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css"
+          integrity="sha512-0S+nbAYis87iX26mmj/+fWt1MmaKCv80H+Mbo+Ne7ES4I6rxswpfnC6PxmLiw33Ywj2ghbtTw0FkLbMWqh4F7Q=="
+          crossorigin="anonymous"/>
+
+    <!-- AdminLTE -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.0.5/css/adminlte.min.css"
+          integrity="sha512-rVZC4rf0Piwtw/LsgwXxKXzWq3L0P6atiQKBNuXYRbg2FoRbSTIY0k2DxuJcs7dk4e/ShtMzglHKBOJxW8EQyQ=="
+          crossorigin="anonymous"/>
+
+    <!-- iCheck -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/icheck-bootstrap/3.0.1/icheck-bootstrap.min.css"
+          integrity="sha512-8vq2g5nHE062j3xor4XxPeZiPjmRDh6wlufQlfC6pdQ/9urJkU07NM0tEREeymP++NczacJ/Q59ul+/K2eYvcg=="
+          crossorigin="anonymous"/>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+ 
+</head>
+<body>
+
+<div class="container" style="margin-top:100px;">
+
+
     <div class="row justify-content-center">
         <div class="col-md-5">
+
+            <h3 style="font-size: 28px; margin:auto; position:relative; left: 45px; top: 8px; color: black; ">Guidance Information System <br>
+               <br>
+            </h3>
+
             <div class="card shadow-lg rounded" >
-                <div class="card-header" >
-                   <h1 style="font-size: 20px;">Login</h1>
+                <div class="card-header bg-dark shadow-lg rounded" >
+
+                    <h3 style="font-size: 18px; margin:auto; position:relative; left: 27px; top: 8px; color: white ">PANGANGAN NATIONAL HIGH SCHOOL <br>
+                        <p style="font-size: 15px; align-text:center; margin:auto; position:relative; left: 100px; color: whitesmoke;">Talisay, Calape, Bohol</p> <br>
+                    </h3>
+                
                 </div>
 
                 <div class="card-body">
@@ -79,6 +127,15 @@
                 </div>
             </div>
         </div>
+
     </div>
 </div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.0.5/js/adminlte.min.js"
+        integrity="sha512-++c7zGcm18AhH83pOIETVReg0dr1Yn8XTRw+0bWSIWAVCAwz1s2PwnSj4z/OOyKlwSXc4RLg3nnjR22q0dhEyA=="
+        crossorigin="anonymous"></script>
+
+</body>
+
+</html>
+
 @endsection
